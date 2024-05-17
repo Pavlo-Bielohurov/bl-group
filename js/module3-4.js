a52a7bd6af82bc60a9dfe112d17aa0c8f1574c9d
Vladyslav112 <lagunetsvlad@gmail.com>
2024-05-17 20:58:13 +0300

    8

--- js/module3-4.js
+++ js/module3-4.js
@@ -48,9 +48,46 @@
 // function totalArr(array) {
 //     const result = [];
 //     for (let i = 0; i < array.length -1; i+=1) {
-//         result.push(array[i] + array[i+1]); 
-        
+//         result.push(array[i] + array[i+1]);
+
 //     }
 //     return result;
 // }
 // console.log(totalArr(someArr));
+
+//TODO: № 7 ==============================================================
+// В нас є объект, в якому зберігаються зарплатні
+// нашої команди
+// Напишіть код для підсумовання всіх зарплат і
+// Збережіть його результат в перемінну sum.
+// Якщо объект salaries пустий, тоді результат має бути 0
+// const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
+// let totalSalaries = 0;
+// const salari = Object.values(salaries);
+// for (const sala of salari) {
+//   totalSalaries += sala;
+// }
+// // for (const sala of Object.values(salaries)) {
+// //   totalSalaries += sala;
+// // } // Bez zminoy
+
+// console.log(totalSalaries);
+
+//TODO: № 8 ================================================================
+// Потрібно написати функцію, яка приймає 2 параметри key та obj,
+// яка буде перебирати об'єкт
+// Якщо у об'єкта є такий ключ - поверне true
+// const obj = {
+//   name: "Igor",
+//   car: "Mercedes",
+//   carColor: "black",
+// };
+// function getBool(key, obj) {
+//   //   return Object.keys(obj).includes(key);
+//   return key in obj; // for in
+// }
+// console.log(getBool("name", obj));
