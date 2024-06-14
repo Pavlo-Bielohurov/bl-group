b84dcd23d960ed390e472cc135be2745f95092ea
Serhii Tyron <mestn1y@hotmail.com>
2024-06-14 20:25:40 +0300

    module 7-8 task3

---
+++ js/module7-8.js
@@ -0,0 +1,13 @@
+//TODO: № 3 ==============================================
+// При кліку на кнопку текст параграфів змінюється на їх порядковий номер
+
+// const paragraphs = document.querySelectorAll("p");
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", getElement);
+
+// function getElement() {
+//   paragraphs.forEach((p, index) => {
+//     p.textContent = index + 1;
+//   });
+// }
